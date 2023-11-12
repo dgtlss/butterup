7c57892e805a4a3de139f3f9fc5bce1ea795cbf0
Nathan Langer <32520453+dgtlss@users.noreply.github.com>
2023-11-12 12:12:34 +0000

    Add options for toast customization

--- butterup.js
+++ butterup.js
@@ -1,7 +1,13 @@
 var butterup = {
-    toast:function(title, message, type, location, icon){
+    toast:function(title, message, type, location, icon, customIcon){
         
     }
 }
 
 
+/* Options for a user to spawn a toast are as follows:
+    - location: top-left, top-right, bottom-left, bottom-right, top-center, bottom-center
+    - type: success, error, warning, info, (if no type is specified, it will default to standard)
+    - icon: boolean (true or false)
+    - customIcon: string (path to custom icon) ** If icon is set to false, this will be ignored **
+*/
