09386e6f4293ff337aa753af0f0bc159b93210ed
Nathan Langer <32520453+dgtlss@users.noreply.github.com>
2023-11-13 00:29:07 +0000

    Update toast function to include icons and location options

--- butterup.js
+++ butterup.js
@@ -4,10 +4,7 @@ var butterup = {
         toastLife: 5000, // How long a toast will stay on the screen before fading away
         currentToasts: 0, // Current number of toasts on the screen
     },
-    toast:function({title, message, type, location, icon, customIcon}){
-        // we need to spawn the toaster inside the body depending on the location specified. If no location is specified then we spawn in the top-right by default
-        // the div has an ol inside of it with a class of rack. each li inside of this has a class of butteruptoast
-        
+    toast:function({title, message, type, location, icon, customIcon}){        
         /* Check if the toaster exists. If it doesn't, create it. If it does, check if there are too many toasts on the screen.
         If there are too many, delete the oldest one and create a new one. If there aren't too many, create a new one. */
         if(document.getElementById('toaster') == null){
@@ -60,6 +57,38 @@ var butterup = {
         // Add the toast to the rack
         document.getElementById('butterupRack').appendChild(toast);
 
+        // check if the user wants an icon
+        if(icon != null){
+            // add a div inside the toast with a class of icon
+            const toastIcon = document.createElement('div');
+            toastIcon.className = 'icon';
+            toast.appendChild(toastIcon);
+            if(type != null){
+                // add the type class to the toast
+                toast.className += ' ' + type;
+                if(type == 'success'){
+                    toastIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">'+
+                    '<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />'+
+                    '</svg>';
+                }
+                if(type == 'error'){
+                    toastIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">'+
+                    '<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd" />'+
+                    '</svg>'
+                }
+                if(type == 'warning'){
+                    toastIcon.innerHTML = ' <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">'+
+                    '<path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />'+
+                    '</svg>'
+                }
+                if(type == 'info'){
+                    toastIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">'+
+                    '<path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" />'+
+                    '</svg>'
+                }
+            }
+        }  
+
         // add a div inside the toast with a class of notif
         const toastNotif = document.createElement('div');
         toastNotif.className = 'notif';
@@ -70,6 +99,7 @@ var butterup = {
         toastDesc.className = 'desc';
         toastNotif.appendChild(toastDesc);
 
+        // check if the user added a title
         if(title != null){
             const toastTitle = document.createElement('div');
             toastTitle.className = 'title';
@@ -77,13 +107,13 @@ var butterup = {
             toastDesc.appendChild(toastTitle);
         }
 
+        // check if the user added a message
         if(message != null){
             const toastMessage = document.createElement('div');
             toastMessage.className = 'message';
             toastMessage.innerHTML = message;
             toastDesc.appendChild(toastMessage);
         }
-
 
 
         // remove the entrance animation class after the animation has finished
