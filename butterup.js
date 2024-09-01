f5ba9840f0003d78d69050990b0d1f93f4e80314
Nathan Langer <32520453+dgtlss@users.noreply.github.com>
2024-09-01 17:23:16 +0100

    promise toasts

--- butterup.js
+++ butterup.js
@@ -56,6 +56,8 @@ var butterup = {
         const toast = document.createElement('li');
         butterup.options.currentToasts++;
         toast.className = 'butteruptoast';
+        // Add entrance animation class
+        toast.className += ' toast-enter';
         // if the toast class contains a top or bottom location, add the appropriate class to the toast
         if(toaster.className.includes('top-right') || toaster.className.includes('top-center') || toaster.className.includes('top-left')){
             toast.className += ' toastDown';
@@ -202,12 +204,11 @@ var butterup = {
             });
         }
   
-        // remove the entrance animation class after the animation has finished
+        // Remove the entrance animation class after the animation has finished
         setTimeout(function(){
-            toast.className = toast.className.replace(' toastDown', '');
-            toast.className = toast.className.replace(' toastUp', '');
-        }, 500);
-  
+            toast.classList.remove('toast-enter');
+        }, 300); // Adjust timing as needed
+
         // despawn the toast after the specified time
         setTimeout(function(){
           if(onTimeout && typeof onTimeout === 'function') {
@@ -218,21 +219,16 @@ var butterup = {
   
     },
     despawnToast(toastId, onClosed){
-        // fade out the toast and then remove it from the DOM
         var toast = document.getElementById(toastId);
-        // does the toast exist?
         if(toast != null){
-            toast.className += ' fadeOutToast';
+            toast.classList.add('toast-exit');
             setTimeout(function(){
-                // set the opacity to 0
                 try{
-                    toast.style.opacity = '0';
                     toast.parentNode.removeChild(toast);
                     butterup.options.currentToasts--;
-                      // Call onClosed callback if provided
-                      if(onClosed && typeof onClosed === 'function') {
-                          onClosed(toast);
-                      }
+                    if(onClosed && typeof onClosed === 'function') {
+                        onClosed(toast);
+                    }
                 }catch(e){
                     // do nothing
                 }
@@ -241,7 +237,77 @@ var butterup = {
                     var toaster = document.getElementById('toaster');
                     toaster.parentNode.removeChild(toaster);
                 }
-            }, 500);
+            }, 300); // Adjust timing to match your CSS animation duration
         }
-    }
-  };
+    },
+    promise: function({promise, loadingMessage, successMessage, errorMessage, location, theme}) {
+        const toastId = 'butterupToast-' + (butterup.options.currentToasts + 1);
+        
+        // Create initial loading toast
+        this.toast({
+            message: loadingMessage || 'Loading...',
+            location: location,
+            theme: theme,
+            icon: true,
+            customIcon: '<svg class="animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>',
+            dismissable: false
+        });
+
+        // Update toast based on promise outcome
+        return promise.then(
+            (result) => {
+                this.updatePromiseToast(toastId, {
+                    type: 'success',
+                    message: successMessage || 'Operation successful',
+                    icon: true
+                });
+                return result;
+            },
+            (error) => {
+                this.updatePromiseToast(toastId, {
+                    type: 'error',
+                    message: errorMessage || 'An error occurred',
+                    icon: true
+                });
+                throw error;
+            }
+        );
+    },
+    updatePromiseToast: function(toastId, {type, message, icon}) {
+        const toast = document.getElementById(toastId);
+        if (toast) {
+            toast.className = toast.className.replace(/success|error|warning|info/g, '');
+            toast.classList.add(type);
+
+            const messageEl = toast.querySelector('.message');
+            if (messageEl) {
+                messageEl.textContent = message;
+            }
+
+            const iconEl = toast.querySelector('.icon');
+            if (iconEl && icon) {
+                iconEl.innerHTML = this.getIconForType(type);
+            }
+
+            // Reset the toast lifetime
+            clearTimeout(toast.timeoutId);
+            toast.timeoutId = setTimeout(() => {
+                this.despawnToast(toastId);
+            }, this.options.toastLife);
+        }
+    },
+    getIconForType: function(type) {
+        switch(type) {
+            case 'success':
+                return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>';
+            case 'error':
+                return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd" /></svg>';
+            case 'warning':
+                return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" /></svg>';
+            case 'info':
+                return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" /></svg>';
+            default:
+                return '';
+        }
+    },
+};
